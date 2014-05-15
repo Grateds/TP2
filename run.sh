08f336e7a917bde6169a10d0d41d7d9678e74302
vercryger <abuzaid.karim@gmail.com>
2014-05-15 12:13:14 -0300

    Added colored script

--- run.sh
+++ run.sh
@@ -26,39 +26,42 @@ plugdash=${blue}
 plugname=${bold}${blue}
  
 # Wrapper function for Maven's mvn command.
-mvn-color()
-{
-# Filter mvn output using sed
-mvn $@ | sed -e "s/\(\[INFO\]\) Building \(.*\)/${info}\1${end} ${projname}Building \2${end}/g" \
--e "s/\(\[INFO\]\) \(\-\{1,\}\) \(.*\) \(\-\{1,\}\)/${info}\1${end} ${plugdash}\2${end} ${plugname}\3${end} ${plugdash}\4${end}/g" \
--e "s/\(\[INFO\]\) \(\-*\)/${info}\1${end} ${projdash}\2${end}/g" \
--e "s/\(\[DEBUG\].*\)/${debug}\1${end}/g" \
--e "s/\(\[WARNING\].*\)/${warn}\1${end}/g" \
--e "s/\(\[ERROR\].*\)/${error}\1${end}/g" \
--e "s/SUCCESS \(\[[0-9]*.[0-9]*s\]\)/${success}SUCCESS \1${end}/g" \
--e "s/FAILURE \(\[[0-9]*.[0-9]*s\]\)/${error}FAILURE \1${end}/g" \
--e "s/SKIPPED/${skipped}SKIPPED${end}/g" \
--e "s/BUILD FAILURE/${error}BUILD FAILURE${end}/g" \
--e "s/BUILD SUCCESS/${success}BUILD SUCCESS${end}/g" \
--e "s/\(<<< FAILURE!\)/${error}\1${end}/g" \
--e "s/\(<<< ERROR!\)/${error}\1${end}/g" \
--e "s/Caused by: \(.*\)/${white}${bg_red}Caused by: \1${end}/g" \
--e "s/Tests run: \([0-9]*\), Failures: \([0-9]*\), Errors: \([0-9]*\), Skipped: \([0-9]*\)/${success}Tests run: \1 ${end}, ${warn}Failures: \2 ${end}, ${error}Errors: \3 ${end}, ${skipped}Skipped: \4 ${end}/g"
+mvn-color() {
+    # Filter mvn output using sed
+    mvn $@ | sed -e "s/\(\[INFO\]\) Building \(.*\)/${info}\1${end} ${projname}Building \2${end}/g" \
+    -e "s/\(\[INFO\]\) \(\-\{1,\}\) \(.*\) \(\-\{1,\}\)/${info}\1${end} ${plugdash}\2${end} ${plugname}\3${end} ${plugdash}\4${end}/g" \
+    -e "s/\(\[INFO\]\) \(\-*\)/${info}\1${end} ${projdash}\2${end}/g" \
+    -e "s/\(\[DEBUG\].*\)/${debug}\1${end}/g" \
+    -e "s/\(\[WARNING\].*\)/${warn}\1${end}/g" \
+    -e "s/\(\[ERROR\].*\)/${error}\1${end}/g" \
+    -e "s/SUCCESS \(\[[0-9]*.[0-9]*s\]\)/${success}SUCCESS \1${end}/g" \
+    -e "s/FAILURE \(\[[0-9]*.[0-9]*s\]\)/${error}FAILURE \1${end}/g" \
+    -e "s/SKIPPED/${skipped}SKIPPED${end}/g" \
+    -e "s/BUILD FAILURE/${error}BUILD FAILURE${end}/g" \
+    -e "s/BUILD SUCCESS/${success}BUILD SUCCESS${end}/g" \
+    -e "s/\(<<< FAILURE!\)/${error}\1${end}/g" \
+    -e "s/\(<<< ERROR!\)/${error}\1${end}/g" \
+    -e "s/Caused by: \(.*\)/${white}${bg_red}Caused by: \1${end}/g" \
+    -e "s/Tests run: \([0-9]*\), Failures: \([0-9]*\), Errors: \([0-9]*\), Skipped: \([0-9]*\)/${success}Tests run: \1 ${end}, ${warn}Failures: \2 ${end}, ${error}Errors: \3 ${end}, ${skipped}Skipped: \4 ${end}/g"
  
-# Make sure formatting is reset
-echo -ne ${end}
+    # Make sure formatting is reset
+    echo -ne ${end}
 }
 
+# Getting scripting
 
-echo "******************************  BUILDING MODULE  ******************************"
+echo -e "${yellow}******************************  BUILDING MODULE  ******************************${end}"
+
 mvn-color clean install
 
-echo "**************************  COLLECTING DEPENDENCIES  **************************"
+echo -e "${yellow}**************************  COLLECTING DEPENDENCIES  **************************${end}"
+
 mvn-color dependency:copy-dependencies
 export CLASPATH=""
 for file in `ls target/dependency`; do export CLASSPATH=$CLASSPATH:target/dependency/$file; done
 export CLASSPATH=$CLASSPATH:target/classes
 
-echo "*****************************  EXECUTING PROGRAM  *****************************"
+echo -e "${yellow}*****************************  EXECUTING PROGRAM  *****************************${end}"
+
 java -cp $CLASSPATH -Dactivejdbc.log com.Grateds.game.App
 
