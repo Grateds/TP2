8c1b4f7e18e93ad616f20ff51f9c77ea9a244d58
vercryger <abuzaid.karim@gmail.com>
2014-05-15 01:19:20 -0300

    Added TestSuite

---
+++ src/test/java/com/Grateds/game/TestSuite.java
@@ -0,0 +1,14 @@
+package com.Grateds.game;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import com.Grateds.game.MODEL.BoardTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AppTest.class, BoardTest.class })
+public class TestSuite {
+
+	/* empty class */
+
+}
