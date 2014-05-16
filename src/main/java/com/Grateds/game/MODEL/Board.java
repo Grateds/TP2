d7c75ec51fff540508e29cd0d8c2c76daf4c1eb6
Dario <astorgadm@gmail.com>
2014-05-16 00:54:42 -0300

    Class Board: movingUp
    movingUp
            subValidMotion
            pasaArregloAMatriz

--- src/main/java/com/Grateds/game/MODEL/Board.java
+++ src/main/java/com/Grateds/game/MODEL/Board.java
@@ -65,4 +65,85 @@ public class Board {
 		}
 		return val;
 	}
+	
+	/**
+	 * 
+	 */
+	public void movingUp(){ 
+		int [] c0 = {this.board[0][0], this.board[1][0], this.board[2][0], this.board[3][0]}; //Columna cero		
+		int [] c1 = {this.board[0][1], this.board[1][1], this.board[2][1], this.board[3][1]}; 	
+		int [] c2 = {this.board[0][2], this.board[1][2], this.board[2][2], this.board[3][2]};		
+		int [] c3 = {this.board[0][3], this.board[1][3], this.board[2][3], this.board[3][3]};	   
+	    subValidMotion(c0);
+	    subValidMotion(c1);
+	    subValidMotion(c2);
+	    subValidMotion(c3);  
+	    pasaArregloAMatriz(c0,c1,c2,c3);
+	}
+	
+	/**
+	 * 
+	 */
+	public void movingBelow(){ 
+		//TODO Implement this method
+	}
+	
+	/**
+	 * 
+	 */
+	public void movingRight(){ 
+		//TODO Implement this method
+	}
+	
+	/**
+	 * 
+	 */
+	public void movingLeft(){
+		//TODO Implement this method
+	}
+	
+	/**
+	 * 
+	 * @param c
+	 */
+	public void subValidMotion(int [] c){
+		for (int m = 0; m < 4; m++) {
+			int i = 0;
+			int j = 1;
+			while (i < j && j < 4) {
+				if (c[i] == 0){
+					c[i] = c[j];
+			    	c[j] = 0;
+			    }
+			    if (c[i] == c[j]){
+			    	c[i] += c[j];
+			    	c[j] = 0;
+			    }
+			    i = j;
+			    j++;
+			}	
+		}
+	}
+	
+	/**
+	 * 
+	 * @param c0
+	 * @param c1
+	 * @param c2
+	 * @param c3
+	 */
+	public void pasaArregloAMatriz(int [] c0, int [] c1, int [] c2, int [] c3){
+		for (int k = 0; k < 4; k++) {
+			this.board[k][0] = c0[k];
+		}
+		for (int k = 0; k < 4; k++) {
+			this.board[k][1] = c1[k];
+		}
+		for (int k = 0; k < 4; k++) {
+			this.board[k][2] = c2[k];
+		}
+		for (int k = 0; k < 4; k++) {
+			this.board[k][3] = c3[k];
+		}
+	}
 }
