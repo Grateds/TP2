e48ec441a5902d6239fac80bdb0ebcb4e17c5570
Dario <astorgadm@gmail.com>
2014-05-16 02:39:32 -0300

    Update App.java

--- src/main/java/com/Grateds/game/App.java
+++ src/main/java/com/Grateds/game/App.java
@@ -13,15 +13,25 @@ public class App {
         
         // PRUEBA A LO CHASTRIN
         Board b = new Board();
-        b.set(0, 0, 0);
-        b.set(0, 2, 4);
-        b.set(0, 3, 8);
-        b.set(1, 3, 16);
-        b.set(2, 2, 4);
-        b.set(2, 3, 32);
-        b.set(3, 2, 4);
-        b.set(3, 3, 0);
-        b.set(3, 0, 2);
+         b.set(0, 1, 2);
+         b.set(1, 1, 2);
+         b.set(1, 2, 8);
+         b.set(1, 3, 4);
+         b.set(2, 2, 16);
+         b.set(2, 3, 16);
+         b.set(3, 1, 2);
+         b.set(3, 2, 8);
+         b.set(3, 3, 8);
+                  
+//        b.set(0, 0, 0);
+//        b.set(0, 2, 4);
+//        b.set(0, 3, 8);
+//        b.set(1, 3, 16);
+//        b.set(2, 2, 4);
+//        b.set(2, 3, 32);
+//        b.set(3, 2, 4);
+//        b.set(3, 3, 0);
+//        b.set(3, 0, 2);
         // Matriz antes
         for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
@@ -31,7 +41,8 @@ public class App {
 		}
         System.out.println(" ");
         
-        b.movingUp();
+        //b.movingUp();
+        b.movingBelow();
         
         //Matriz despues
         for (int i = 0; i < 4; i++) {
