eae4315702a8a0ee7880ddaf7f35350c7dcfadb3
Dario <astorgadm@gmail.com>
2014-05-16 01:03:19 -0300

    Class App: Test to chastrin

--- src/main/java/com/Grateds/game/App.java
+++ src/main/java/com/Grateds/game/App.java
@@ -1,5 +1,7 @@
 package com.Grateds.game;
 
+import com.Grateds.game.MODEL.Board;
+
 /**
  * Hello world!
  *
@@ -8,5 +10,35 @@ public class App {
 	
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        
+        // PRUEBA A LO CHASTRIN
+        Board b = new Board();
+        b.set(0, 0, 0);
+        b.set(0, 2, 4);
+        b.set(0, 3, 8);
+        b.set(1, 3, 16);
+        b.set(2, 2, 4);
+        b.set(2, 3, 32);
+        b.set(3, 2, 4);
+        b.set(3, 3, 0);
+        b.set(3, 0, 2);
+        // Matriz antes
+        for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(b.get(i,j)+ " ");
+			}
+			System.out.println(" ");
+		}
+        System.out.println(" ");
+        
+        b.movingUp();
+        
+        //Matriz despues
+        for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(b.get(i,j)+ " ");
+			}
+			System.out.println(" ");
+		}
     }
 }
