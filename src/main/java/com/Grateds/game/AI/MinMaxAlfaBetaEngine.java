5ef44ac699e30809f36a0ebfcfcf15e51f80d3ac
Dario Astorga <astorgadm@gmail.com>
2014-05-21 21:46:58 -0300

    Changed to adversary

--- src/main/java/com/Grateds/game/AI/MinMaxAlfaBetaEngine.java
+++ src/main/java/com/Grateds/game/AI/MinMaxAlfaBetaEngine.java
@@ -1,5 +1,4 @@
 package com.Grateds.game.AI;
-import java.util.*;
 
 /**
  * Title:        MinMaxAlfaBetaEngine<p>
@@ -13,28 +12,105 @@ import java.util.*;
  */
 
 
-public class MinMaxAlfaBetaEngine<S extends State, Problem extends AbstractSearchProblem<S>> extends AbstractSearchEngine<S,Problem> {
+@SuppressWarnings("hiding")
+public class MinMaxAlfaBetaEngine<P extends AdversarySearchProblem<State>, State extends AdversarySearchState>  extends AdversarySearchEngine<P, State> {
 
+	/** 
+	 * Constructor for abstract class AdversarySearchEngine 
+	 * @pre. true.
+	 * @post. This constructor sets maxDepth to 1.
+	 */
 	public MinMaxAlfaBetaEngine() {
-		super();
+		maxDepth = 1;
+	};
+    
+	/** 
+	 * Constructor for abstract class AbstractSearchEngine.
+	 * @param p is the abstract search problem associated with the engine
+	 * being created.
+	 * @pre. p!=null.
+	 * @post. A reference to p is stored in field problem and maxDepth
+	 * is set to 1.
+	 */	
+    public MinMaxAlfaBetaEngine(P p) {
+        problem = p;
+        maxDepth = 1;
+    }
+    
+	/** 
+	 * Constructor for abstract class AbstractSearchEngine.
+	 * @param p is the abstract search problem associated with the engine
+	 * being created.
+	 * @param maxDepth is a value to set this.maxDepth to.
+	 * @pre. p!=null and maxDepth>=1.
+	 * @post. A reference to p is stored in field problem and 
+	 * this.maxDepth is set to maxDepth.
+	 */	
+    public MinMaxAlfaBetaEngine(P p, int maxDepth) {
+        problem = p;
+        this.maxDepth = maxDepth;
+    }
+    
+	/** 
+	 * Returns the maximum depth to be used for search. This value
+	 * indicates how many game moves in the future are going to be
+	 * explored in order to compute the value for a state.
+	 * @return the maximum depth to be used for search.
+	 * @pre. true.
+	 * @post. the value of maxDepth is returned.
+	 */	   
+	public int getMaxDepth() {
+		return maxDepth;
 	}
-	
-	public MinMaxAlfaBetaEngine(Problem p) {
-		super(p);
+
+	/** 
+	 * Sets the maximum depth to be used for search. This value
+	 * indicates how many game moves in the future are going to be
+	 * explored in order to compute the value for a state.
+	 * @param maxDepth is the value used to set this.maxDepth. 
+	 * @pre. maxDepth >= 1.
+	 * @post. this.maxDepth is set to maxDepth.
+	 */	 
+	public void setMaxDepth(int maxDepth) {
+		this.maxDepth = maxDepth;
 	}
-	
-	public boolean performSearch() {
-		//TODO Implement this method
-		return false;
 
-	} // end of method performSearch
+	/** 
+	 * Returns the problem associated with this search engine.
+	 * @return a reference to the problem associated with this
+	 * search engine. 
+	 * @pre. true.
+	 * @post. this.problem is returned.
+	 */	 
+	public P getProblem() {
+		return problem;
+	}
 
-	public List<S> getPath() {
-		//TODO Implement this method
+	/** 
+	 * Sets the problem associated with the search engine.
+	 * @param p is the search problem to be used for search (to set 'problem' to).
+	 * @pre. p!=null.
+	 * @post. 'problem' is set to p.
+	 */	
+    public void setProblem(P p) {
+        problem = p;
+    }
+
+	@Override
+	public int computeValue(State state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public State computeSuccessor(State state) {
+		// TODO Auto-generated method stub
 		return null;
-	} // end of getPath()
+	}
 
+	@Override
 	public void report() {
-		//TODO Implement this method
-	} // end of report()
+		// TODO Auto-generated method stub
+		
+	}
 } // end of class DepthFirst
