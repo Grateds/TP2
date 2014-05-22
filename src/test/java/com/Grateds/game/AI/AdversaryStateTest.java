5a0a08c54b4729549774fc5a1f34a47fab6cc0da
Dario Astorga <astorgadm@gmail.com>
2014-05-21 21:47:23 -0300

    New test class

---
+++ src/test/java/com/Grateds/game/AI/AdversaryStateTest.java
@@ -0,0 +1,49 @@
+package com.Grateds.game.AI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdversaryStateTest {
+
+	@Test
+	public void testAdversaryStateBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsAdversarySearchState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsSucess() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSucessMoveUp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSucessMoveBellow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSucessMoveRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSucessMoveLeft() {
+		fail("Not yet implemented");
+	}
+
+}
