10316fbde9de823561ffcba9c9820cbb1e09b829
Dario Astorga <astorgadm@gmail.com>
2014-05-22 22:00:35 -0300

    Update TestSuite

--- src/test/java/com/Grateds/game/TestSuite.java
+++ src/test/java/com/Grateds/game/TestSuite.java
@@ -5,10 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import com.Grateds.game.AI.AdversaryStateTest;
 import com.Grateds.game.MODEL.BoardTest;
-import com.Grateds.game.MODEL.GameStateTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AppTest.class, BoardTest.class, GameStateTest.class, AdversaryStateTest.class })
+@SuiteClasses({ AppTest.class, BoardTest.class, AdversaryStateTest.class })
 public class TestSuite {
 	
 	/* empty class */
