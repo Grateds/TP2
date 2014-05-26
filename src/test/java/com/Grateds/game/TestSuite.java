f58089202d33aad83f7e4dd0efe70ede27c99541
vercryger <abuzaid.karim@gmail.com>
2014-05-26 14:06:33 -0300

    added test class

--- src/test/java/com/Grateds/game/TestSuite.java
+++ src/test/java/com/Grateds/game/TestSuite.java
@@ -3,11 +3,12 @@ package com.Grateds.game;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import com.Grateds.game.AI.AdversaryProblemTest;
 import com.Grateds.game.AI.AdversaryStateTest;
 import com.Grateds.game.MODEL.BoardTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AppTest.class, BoardTest.class, AdversaryStateTest.class })
+@SuiteClasses({ AppTest.class, BoardTest.class, AdversaryStateTest.class, AdversaryProblemTest.class })
 public class TestSuite {
 	
 	/* empty class */
